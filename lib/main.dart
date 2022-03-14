6821b57009d697ca130eb28ae474fbe0665221f6
neel-joflee <neel.vaghani@joflee.com>
2022-03-14 16:02:09 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:post_api_demo/api_service.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Api Service'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ApiService.myApiService();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/model.dart
@@ -0,0 +1,85 @@
+import 'dart:convert';
+
+LoginData loginDataFromJson(String str) => LoginData.fromJson(json.decode(str));
+
+String loginDataToJson(LoginData data) => json.encode(data.toJson());
+
+class LoginData {
+  LoginData({
+    required this.status,
+    required this.message,
+    this.data,
+    required this.code,
+  });
+
+  bool status;
+  String message;
+  Data? data;
+  int code;
+
+  factory LoginData.fromJson(Map<String, dynamic> json) => LoginData(
+        status: json["status"],
+        message: json["message"],
+        data: Data.fromJson(json["data"]),
+        code: json["code"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "status": status,
+        "message": message,
+        "data": data?.toJson(),
+        "code": code,
+      };
+}
+
+class Data {
+  Data({
+    this.id,
+    this.employeeId,
+    this.name,
+    this.email,
+    this.isCompleted,
+    this.token,
+    this.xHasuraUserId,
+    this.xHasuraRole,
+    this.xHasuraIsOwner,
+    this.cacheControl,
+  });
+
+  String? id;
+  String? employeeId;
+  String? name;
+  String? email;
+  bool? isCompleted;
+  String? token;
+  String? xHasuraUserId;
+  String? xHasuraRole;
+  bool? xHasuraIsOwner;
+  String? cacheControl;
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+        id: json["id"],
+        employeeId: json["employee_id"],
+        name: json["name"],
+        email: json["email"],
+        isCompleted: json["is_completed"],
+        token: json["token"],
+        xHasuraUserId: json["X-Hasura-User-Id"],
+        xHasuraRole: json["X-Hasura-Role"],
+        xHasuraIsOwner: json["X-Hasura-Is-Owner"],
+        cacheControl: json["Cache-Control"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "employee_id": employeeId,
+        "name": name,
+        "email": email,
+        "is_completed": isCompleted,
+        "token": token,
+        "X-Hasura-User-Id": xHasuraUserId,
+        "X-Hasura-Role": xHasuraRole,
+        "X-Hasura-Is-Owner": xHasuraIsOwner,
+        "Cache-Control": cacheControl,
+      };
+}
